3d05969134306dd36ced9dc207f12297f9f34426
Fernando <fscasas05@gmail.com>
2025-10-30 19:03:09 +0000

    Add: Post Model

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from utils.rands import slugify_new
 
@@ -53,5 +54,54 @@ class Page(models.Model):
             self.slug = slugify_new(self.title, 4)
         return super().save(*args, **kwargs)
     
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="", null=False, blank=True, max_length=255,
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(
+        default=False,
+        help_text="This post will be visible to everyone if checked.",
+    )
+    content = models.TextField()
+    cover = models.ImageField(
+        upload_to='posts/%Y/%m/', blank=True, default=''
+    )
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text="If checked, the cover image will also appear in the post content.",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_created_by',   
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_updated_by',
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
