13cb605653387aba39f280deb024b4c7cf912458
Fernando <fscasas05@gmail.com>
2025-10-30 18:37:48 +0000

    Add: Model - Page

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -16,6 +16,9 @@ class Tag(models.Model):
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
     
+    def __str__(self):
+        return self.name
+    
 class Category(models.Model):
     class Meta:
         verbose_name = "Category"
@@ -30,3 +33,25 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="", null=False, blank=True, max_length=255,
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text="This page will be visible to everyone if checked.",
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
